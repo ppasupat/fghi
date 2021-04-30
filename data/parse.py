7c335e3634d79579ce1437bde844c4de9d5c5879
Panupong (Ice) Pasupat <panuponggong@gmail.com>
2021-04-29 23:13:34 -0700

    Added common words from 2 sources

--- data/parse.py
+++ data/parse.py
@@ -6,6 +6,9 @@ from collections import defaultdict, Counter
 import xml.etree.ElementTree as ET
 
 
+BCC_TOTAL = 18577726082
+
+
 def parse_svg(char):
   filename = 'raw/makemeahanzi/svgs-still/{}-still.svg'.format(ord(char))
   tree = ET.parse(filename)
@@ -17,18 +20,56 @@ def parse_svg(char):
   return paths
 
 
-def is_legal(char):
+def is_legal_char(char):
   x = ord(char)
   return 0x4e00 <= x <= 0x9fff
+
+
+def yield_from_subtlex():
+  with open('raw/subtlex-ch/SUBTLEX_CH_131210_CE.utf8', encoding='utf-8-sig') as fin:
+    header = fin.readline().rstrip('\n').split('\t')
+    for line in fin:
+      line = dict(zip(header, line.rstrip('\n').split('\t')))
+      word = line['Word']
+      freq = float(line['W.million'])
+      yield (freq, word)
+
+
+def yield_from_bcc():
+  with open('raw/bcc/bcc.txt') as fin:
+    for line in fin:
+      line = line.rstrip().split('\t')
+      word = line[1]
+      freq = float(line[2]) * 1e6 / BCC_TOTAL
+      yield (freq, word)
+
+
+def yield_common_words():
+  """Yields (freq-per-million, word) in decreasing order."""
+  s1 = iter(yield_from_subtlex())
+  s2 = iter(yield_from_bcc())
+  f1, w1 = next(s1)
+  f2, w2 = next(s2)
+  while f1 > 0 or f2 > 0:
+    if f1 > f2:
+      yield f1, w1
+      try:
+        f1, w1 = next(s1)
+      except StopIteration:
+        f1, w1 = -1, None
+    else:
+      yield f2, w2
+      try:
+        f2, w2 = next(s2)
+      except StopIteration:
+        f2, w2 = -1, None
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-v', '--verbose', action='store_true')
-  parser.add_argument('-r', '--subtlexch-max-rank', type=int, default=999999,
-      help='Maximum rank of common words from SUBTLEX-CH')
-  parser.add_argument('-c', '--subtlexch-max-per-char', type=int, default=10,
-      help='Maximum number of common words from SUBTLEX-CH')
+  parser.add_argument('-m', '--min-common-word-freq', type=float, default=5.,
+      help='Common word filter: minimum number of occurrences per 1 million words')
   args = parser.parse_args()
 
   if not os.path.exists('vocab'):
@@ -38,8 +79,8 @@ def main():
   print('Reading words and chars')
   hsk = [None] * 8
   hsk_so_far = set()
-  seen_words = set()
   char_to_words = defaultdict(list)
+  used_words = set()
   for level in (1,2,3,4,5,6):
     print('HSK level', level)
     hsk[level] = set()
@@ -48,16 +89,17 @@ def main():
         # simp, trad, pinyin_num, pinyin_tone, meaning
         line = line.strip().split('\t')
         word, pron, gloss = line[0], line[3], line[4]
-        seen_words.add(word)
         line = [word, level, pron, gloss]
+        used_words.add(word)
         for char in set(word):
-          if not is_legal(char):
+          if not is_legal_char(char):
             print('Skipping "{}" (U+{})'.format(char, hex(ord(char))))
             continue
           char_to_words[char].append(line)
           if char not in hsk_so_far:
             hsk[level].add(char)
     hsk_so_far.update(hsk[level])
+  print('Added {} chars + {} words.'.format(len(hsk_so_far), len(used_words)))
 
   # Read the extra characters
   with open('hsk.json') as fin:
@@ -66,6 +108,7 @@ def main():
   for level in (1,2,3,4,5,6):
     all_chars -= set(hsk[level])
   hsk[7] = sorted(all_chars)
+  print('Added {} extra chars.'.format(len(hsk[7])))
 
   # Read Unihan
   char_to_infos = defaultdict(dict)
@@ -82,28 +125,37 @@ def main():
       elif key == 'kTGHZ2013':
         char_to_infos[char]['pron'] = ', '.join(x.split(':')[-1] for x in value.split())
 
-  # Read common words from SUBTLEX-CH
-  char_to_extra_words = defaultdict(list)
-  with open('raw/subtlex-ch/SUBTLEX_CH_131210_CE.utf8', encoding='utf-8-sig') as fin:
-    header = fin.readline().rstrip('\n').split('\t')
-    for ii, line in enumerate(fin):
-      if ii >= args.subtlexch_max_rank:
-        break
-      line = dict(zip(header, line.rstrip('\n').split('\t')))
-      word = line['Word']
-      pron = line['Pinyin']
-      gloss = line['Eng.Tran.'].replace('/', '; ')
-      if gloss == '#':
+  # Read CEDict
+  print('Reading CEDict')
+  cedict = defaultdict(list)
+  with open('raw/cc-cedict/cedict_ts.u8') as fin:
+    fin.readline()    # Avoid the BOM
+    for line in fin:
+      if line[0] == '#':
         continue
-      line = [word, ii, pron, gloss]
-      if word not in seen_words:
-        seen_words.add(word)
-        for char in set(word):
-          if not is_legal(char):
-            print('Skipping "{}" (U+{})'.format(char, hex(ord(char))))
-            continue
-          if len(char_to_extra_words[char]) < args.subtlexch_max_per_char:
-            char_to_extra_words[char].append(line)
+      m = re.match(r'^(\S+) (\S+) \[([^]]+)\] /(.*)/$', line.rstrip('\n'))
+      assert m is not None, line
+      word, _, pron, gloss = m.groups()
+      gloss = gloss.replace('/', '; ')
+      cedict[word].append([pron, gloss])
+  print('Read {} words.'.format(len(cedict)))
+
+  # Read common words
+  char_to_extra_words = defaultdict(list)
+  num_extra_words = 0
+  for freq, word in yield_common_words():
+    if freq < args.min_common_word_freq:
+      break
+    if word in used_words or word not in cedict:
+      continue
+    for pron, gloss in cedict[word]:
+      line = [word, round(freq, 2), pron, gloss]
+      for char in set(word):
+        if is_legal_char(char):
+          char_to_extra_words[char].append(line)
+    used_words.add(word)
+    num_extra_words += 1
+  print('Added {} extra words.'.format(num_extra_words))
 
   print('Writing information to vocab/ ...')
   for level, chars in enumerate(hsk):
